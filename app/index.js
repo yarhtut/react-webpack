fb768c3f02437a009b71f587ce30e42ce89e894a
Yar Htut <yar@silverstripe.com>
2016-03-15 20:10:00 +1300

    inital commit

--- app/index.js
+++ app/index.js
@@ -1,1 +1,15 @@
-index.js
+import React from ('react');
+import ReactDOM from ('react-dom');
+
+class HelloWorld extend React.component{
+	render(){
+		return(
+			<div> hello world</div>
+			)
+	}
+}
+
+ReactDom.render(
+	<Helloworld />,
+	document.getElementById('app')
+	);
