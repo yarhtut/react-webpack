398a775eae201bb5faf8e3384d0f1816280b5eb6
Yar Htut <yar@silverstripe.com>
2016-03-15 22:15:39 +1300

    child components

--- app/index.js
+++ app/index.js
@@ -1,15 +1,63 @@
-import React from ('react');
-import ReactDOM from ('react-dom');
-
-class HelloWorld extend React.component{
-	render(){
-		return(
-			<div> hello world</div>
-			)
-	}
+var USER_DATA = {
+	name: "yarhtut",
+	username: 'yarhtut',
+	image: 'https://avatars1.githubusercontent.com/u/8251597?v=3&u=af4b86aa2961a25adecc31ca2c7e408188b24571&s=140'
 }
 
-ReactDom.render(
-	<Helloworld />,
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+/*
+	Focused
+	Independent
+	Reusable
+	Small
+	Testable
+*/
+
+var ProfilePic = React.createClass({
+	render:function() {
+		return(
+			<img src={this.props.imageUrl} style={{height: 100, width: 100}}/>
+		)
+	}
+})
+
+var ProfileLink = React.createClass({
+	render: function() {
+		return(
+			<div>
+				<a href={'https://www.github.com/' + this.props.username}>
+				{this.props.username}
+				</a>
+			</div>
+			)
+	}
+})
+
+var ProfileName = React.createClass({
+	render: function() {
+		return (
+			<div>
+				{this.props.name}
+			</div>
+			)
+	}
+})
+
+var Avatar = React.createClass({
+	render: function () {
+		return(
+			<div>
+				<ProfilePic imageUrl={this.props.user.image}/>
+				<ProfileName name={this.props.user.name} />
+				<ProfileLink username={this.props.user.username}/>
+			</div>
+		)
+	}
+})
+
+ReactDOM.render(
+	<Avatar user={USER_DATA}/>,
 	document.getElementById('app')
-	);
+	)
