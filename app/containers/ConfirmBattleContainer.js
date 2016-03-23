75de3771effc94c4cc9643634ade63d2c10048e6
yar@boost.co.nz <yarhtut@live.com>
2016-03-23 23:12:02 +1300

    add confirmBattleContainer

---
+++ app/containers/ConfirmBattleContainer.js
@@ -0,0 +1,11 @@
+var React = require('react');
+
+var ConfirmBattleContainer = React.createClass({
+	render: function() {
+		return (
+			<div />
+		)
+	}
+})
+
+module.exports = ConfirmBattleContainer;
