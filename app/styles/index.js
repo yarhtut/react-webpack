6bfec8b0865c75d5e7278ae69ad8a23ed450d5ba
yar@boost.co.nz <yarhtut@live.com>
2016-03-25 23:51:31 +1300

    added Results page

--- app/styles/index.js
+++ app/styles/index.js
@@ -1,6 +1,10 @@
-var styles= {
-	transparentBg: {
-		background: 'transparent'
-	}
+var styles = {
+  transparentBg: {
+    background: 'transparent'
+  },
+  space: {
+    marginTop: '25px',
+  }
 }
+
 module.exports = styles;
