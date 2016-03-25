ce21a6b40ed27825310c97afe42068d67d8f0780
yar@boost.co.nz <yarhtut@live.com>
2016-03-26 01:02:39 +1300

    final commit

--- app/config/routes.js
+++ app/config/routes.js
@@ -23,8 +23,3 @@ var routes = (
 );
 
 module.exports = routes;
-
-// <Route path="playerOne" header="Player One" component={PromptContainer}/>
-// <Route path="playerTwo/:playerOne" header="Player Two" component={PromptContainer}/>
-// <Route path="battle" component={ConfirmBattleContainer}/>
-// <Route path="results" component={ResultsContainer}/>
--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,23 @@
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-var HtmlWebpackPluginConfig = new HtmlWebpackPlugin ({
-	template: __dirname + '/app/index.html',
-	filename: 'index.html',
-	inject: 'body'
-})
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+var HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: __dirname + '/app/index.html',
+  filename: 'index.html',
+  inject: 'body'
+});
 
 module.exports = {
-	entry: [
-	'./app/index.js'
-	],
-	output: {
-		path: __dirname + '/dist',
-		filename: "index_bundle.js"
-	},
-	module: {
-		loaders: [
-		{test: /\.js$/, exclude: /node_modules/, loader: "babel-loader"}
-		]
-	},
-	plugins: [HtmlWebpackPluginConfig]
-}
+  entry: [
+    './app/index.js'
+  ],
+  output: {
+    path: __dirname + '/dist',
+    filename: "index_bundle.js"
+  },
+  module: {
+    loaders: [
+      {test: /\.js$/, include: __dirname + '/app', loader: "babel-loader"},
+      { test: /\.css$/, loader: "style-loader!css-loader" }
+    ]
+  },
+  plugins: [HTMLWebpackPluginConfig]
+};
